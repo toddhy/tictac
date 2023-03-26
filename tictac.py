06b460023683b48919cea564cc6508f622b0b0a1
Todd <todd@todd-AB350M-DS3H.pi>
2023-03-25 17:01:56 -1000

    get player input, place an X on board

--- tictac.py
+++ tictac.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-HEIGHT = 5
-WIDTH = 6
+HEIGHT = 3
+WIDTH = 3
 
 def make_board(width, height):
 	board = []
@@ -13,8 +13,29 @@ def make_board(width, height):
 			i += 1
 	return board
 
-a = make_board(HEIGHT, WIDTH)
-print(a)
-print(a[0])
-print(a[0][2])
+def print_board(board):
+	for row in board:
+		print(row)
+
+def player_choice(board):
+	#ask for input and return an int within board range
+	max = len(board) * len(board[0])
+	choice = input("select a square")
+	if choice.isdigit() == False:
+		print("Please enter a number")
+	elif int(choice) > max:
+		print("Number out of range")
+	else:
+		return int(choice)
+
+def record_choice(board, player, choice):
+	if player == 1:
+		board[0][choice] = 'X'
+	return board
+	
+
+board = make_board(HEIGHT, WIDTH)
+print_board(board)
+board = record_choice(board, 1, player_choice(board))
+print_board(board)
 
